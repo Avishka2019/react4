ec2896a40f60ae6bfc75680737f4292a107af721
Avishka2019 <56026135+Avishka2019@users.noreply.github.com>
2020-10-26 20:09:07 +0530

    Update login.component.js

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -12,7 +12,7 @@ import {Redirect} from "react-router-dom";
  			password: this.password
  		}
  		console.log(data);
- 		axios.post('https://avishkadev-api.herokuapp.com/api/auth',data).then(
+ 		axios.post('https://avishka-dev.herokuapp.com/api/auth',data).then(
  			res=>{
  			console.log(res.data.token);
         localStorage.setItem('token', res.data.token);
