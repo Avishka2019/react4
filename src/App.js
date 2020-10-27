91640a372d24e87775e3d941ea85ee3b5b6e188c
Avishka2019 <56026135+Avishka2019@users.noreply.github.com>
2020-10-27 09:49:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React, { Component } from 'react';
 import Post from  "./components/post.component";
 import Home from  "./components/home.component";
 import Product from  "./components/product.component";
-import Login from  "./components/Login.component";
+import Login from  "./components/login.component";
 //import BootstrapModal from  "./components/reactbootstrap.component";
 import {
   BrowserRouter,
